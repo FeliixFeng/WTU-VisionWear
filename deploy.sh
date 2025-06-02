e279f208c35808c5ae36f2d4cbdb716711d5c925
coderxguan <3288733739@qq.com>
2025-06-02 21:21:23 +0800

    修改readme

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,6 @@ echo "编译并部署后端..."
 cd backend
 mvn clean install
 echo "请输入服务器密码以传输后端文件:"
-echo "密码: Hg02190501?"
 scp target/backend-0.0.1-SNAPSHOT.jar root@8.155.5.178:/opt/vision-wear
 
 # 部署前端
@@ -16,11 +15,7 @@ echo "编译并部署前端..."
 cd ../frontend
 npm run build
 echo "请输入服务器密码以传输前端文件:"
-echo "密码: Hg02190501?"
 scp -r dist/ root@8.155.5.178:/opt/vision-wear/
 
 # 重启服务
-echo "文件上传完成! 请手动执行以下命令重启服务:"
-echo "密码: Hg02190501?"
-echo "ssh root@8.155.5.178"
-echo "cd /opt/vision-wear && bash run.sh" 
+echo "文件上传完成! "

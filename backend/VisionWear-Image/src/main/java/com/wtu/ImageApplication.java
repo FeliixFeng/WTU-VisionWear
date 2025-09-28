c52a2ec72e6f243d6a3fdf119e5047addeb9dff1
dghyuiok <14341407+dghyuiok@user.noreply.gitee.com>
2025-09-28 20:23:07 +0800

    打包时跳过测试

--- backend/VisionWear-Image/src/main/java/com/wtu/ImageApplication.java
+++ backend/VisionWear-Image/src/main/java/com/wtu/ImageApplication.java
@@ -3,7 +3,6 @@ package com.wtu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = {"com.wtu.utils"})
 @EnableFeignClients
